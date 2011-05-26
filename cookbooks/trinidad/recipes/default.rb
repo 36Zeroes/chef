f9f818b91e30410822ef7e800e8da6d77faf192a
Adam Davies <adam@36zeroes.com.au>
2011-05-26 17:19:16 +0930

    Need directory as well

--- cookbooks/trinidad/recipes/default.rb
+++ cookbooks/trinidad/recipes/default.rb
@@ -5,6 +5,13 @@ gem_package "trinidad" do
   action :install
 end
 
+directory "/etc/trinidad" do
+  owner "root"
+  group "root"
+  mode 0755
+  action :create
+end
+
 template "/etc/trinidad/config.yaml" do
   source 'config.yaml.erb'
   owner "root"

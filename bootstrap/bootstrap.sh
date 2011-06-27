e3d3ba7123601ca366bd4db7d5e2ba1d8935c794
Adam Davies <adam@36zeroes.com.au>
2011-06-27 16:23:47 +0930

    Indenting code for readability

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -4,45 +4,44 @@
 # BOOTSTRAP CHEF INSTALL
 echo "Adding opscode repository..."
 echo 'deb http://apt.opscode.com/ lucid main' |  tee /etc/apt/sources.list.d/opscode.list
-wget -qO - http://apt.opscode.com/packages@opscode.com.gpg.key |  apt-key add -
+  wget -qO - http://apt.opscode.com/packages@opscode.com.gpg.key |  apt-key add -
 
 
 echo "Updating package list.."
-apt-get update
+  apt-get update
 
 echo "Installing chef.."
-apt-get install chef -y
+  apt-get install chef -y
 echo "done."
 echo ""
 
-# Disable chef-client running, coze we do chef-solo
+# Disable chef-client running, coz we do chef-solo
 echo "Stopping chef-client as chef-solo is all thats used..."
-service chef-client stop
+  service chef-client stop
 echo "Disabling chef-client from boot..."
-update-rc.d chef-client disable
+  update-rc.d chef-client disable
 echo "done."
 echo ""
 
 # CHECKOUT COOKBOOKS from 36Zeroes Chef
-apt-get install git-core -y
+  apt-get install git-core -y
 echo "Putting 36 Zeroes chef repo at /var/chef..."
-mkdir /var/chef
-git clone https://36Zeroes@github.com/36Zeroes/chef.git /var/chef
-cd /var/chef
-git submodule init
-git submodule update
+  mkdir /var/chef
+  git clone https://36Zeroes@github.com/36Zeroes/chef.git /var/chef
+  cd /var/chef
+  git submodule init
+  git submodule update
 echo "done."
 echo ""
 
 # Symlink the config
- ln -s /var/chef/bootstrap/solo.rb /etc/chef/solo.rb
+  ln -s /var/chef/bootstrap/solo.rb /etc/chef/solo.rb
 echo "Linked /etc/chef/solo.rb -> /var/chef/bootstrap/solo.rb"
- ln -s /var/chef/clients /etc/chef/clients
+  ln -s /var/chef/clients /etc/chef/clients
 echo "Linked /etc/chef/clients/ -> /var/chef/clients/"
 echo ""
 
 # Run bootstrap chef-solo
 echo "To run, use your client node name, like so:"
 echo " chef-solo -j /etc/chef/clients/kirra/reporting.json"
-
 

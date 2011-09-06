d473b84d940d21843ddaeb1340c1775c88892118
Adam Davies <adam@36zeroes.com.au>
2011-09-06 15:13:45 +0930

    Add tour_reporter unicorn app, and cleaned up unicorn app tasks

--- cookbooks/unicorn/recipes/apps.rb
+++ cookbooks/unicorn/recipes/apps.rb
@@ -3,16 +3,6 @@ counter = 0
 node[:active_applications].each do |name, config|
   app_root = "/var/www/#{name}"
   defaults = Mash.new({
-    :pid_path => "#{app_root}/shared/pids/unicorn.pid",
-    :worker_count => node[:unicorn][:worker_count],
-    :timeout => node[:unicorn][:timeout],
-    :socket_path => "/tmp/unicorn/#{name}.sock",
-    :backlog_limit => 1,
-    :master_bind_address => '0.0.0.0',
-    :master_bind_port => "36#{counter}00",
-    :worker_listeners => true,
-    :worker_bind_address => '127.0.0.1',
-    :worker_bind_base_port => "37#{counter}01",
     :debug => false,
     
     # Use bundler to handle gem dependencies
@@ -22,8 +12,10 @@ node[:active_applications].each do |name, config|
     :app_root => app_root,
     :enable => true,
     :config_path => "#{app_root}/current/config/unicorn.conf.rb",
-    :use_bundler => false
+
+    :run_as => "capistrano:capistrano"
   })
+  
   
   config_with_defaults = defaults.merge(config)
   runit_service "unicorn-#{name}" do
